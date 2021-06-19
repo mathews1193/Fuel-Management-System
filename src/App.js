e229353d8425a9966ecf95b9d37a7a53433ee00e
mathews1193 <mathews1193@yahoo.com>
2021-06-18 22:40:43 -0500

    setup pages for the web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import './App.css';
+import Home from './containers/Home/Home';
+import Login from './containers/Login/Login';
+import SignUp from './containers/SignUp/SignUp';
+import Fuel from './containers/Fuel-Quote/Fuel';
+import Profile from './containers/Client-Profile/Profile';
+import Dashboard from './containers/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div className='container'>
+              <Route exact path="/" component={Home}/>
+              <Route path="/signup"component={SignUp} />
+              <Route path="/login"component={Login} />
+              <Route path="/fuel-quote"component={Fuel} />
+              <Route path="/client-profile"component={Profile} />
+              <Route path="/dashboard"component={Dashboard} />
+        </div>
+      </Router>
     </div>
   );
 }
