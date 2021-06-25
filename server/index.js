04a3870c6b302265c911e3b15fb7cb330a1f7254
mathews1193 <mathews1193@yahoo.com>
2021-06-25 12:55:07 -0500

    configured mysql db for web app

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password:'',
+    database:'fuel-managment-system'
+})
 
 app.listen(3001,() =>{
     console.log("Cool, Your server is running on port 3001")
