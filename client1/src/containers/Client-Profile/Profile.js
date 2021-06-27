66b5840402fa2f35351310ce7459dc74539779bb
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-06-27 12:28:24 -0500

    added profile

--- client1/src/containers/Client-Profile/Profile.js
+++ client1/src/containers/Client-Profile/Profile.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Profile1 from '../../components/Profile/Profile'
 function Profile() {
     return (
         <div>
-            
+            <Profile1/>
         </div>
     )
 }
