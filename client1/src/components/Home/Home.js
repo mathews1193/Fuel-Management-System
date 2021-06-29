4b59f74c02658f6543c0a064e12cb787b754fadc
mathews1193 <mathews1193@yahoo.com>
2021-06-29 09:44:49 -0500

    updated header style for home page

--- client1/src/components/Home/Home.js
+++ client1/src/components/Home/Home.js
@@ -4,8 +4,8 @@ import "./Home.css";
 
 function Home() {
     const img = [
-        'https://images.pexels.com/photos/3802793/pexels-photo-3802793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        'https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+        'https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+        'https://images.pexels.com/photos/3802793/pexels-photo-3802793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     ]; 
 
     return (
@@ -14,7 +14,7 @@ function Home() {
                 <div className="img-set1">
                     <img className="img1"src={img[0]} alt="Logo" width="350" height="300"/>
                     <div className="title-background">
-                        <h1 className="sub-title1">Taking Your Fuel Managment Need</h1>
+                        <h1 className="sub-title1">Taking Your Fuel Management Need</h1>
                         <h1 className="sub-title1">To</h1>
                         <h1 className="sub-title1">The Next Level!</h1>
                     </div>                    
