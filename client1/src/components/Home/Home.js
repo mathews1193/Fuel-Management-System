bb369e80025d3e50a1c6085a48d66c1f492046eb
mathews1193 <mathews1193@yahoo.com>
2021-07-11 02:21:48 -0500

    updated style of Home and About us page

--- client1/src/components/Home/Home.js
+++ client1/src/components/Home/Home.js
@@ -5,14 +5,14 @@ import "./Home.css";
 function Home() {
     const img = [
         'https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-        'https://images.pexels.com/photos/3802793/pexels-photo-3802793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        'https://images.pexels.com/photos/5411674/pexels-photo-5411674.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     ]; 
 
     return (
         <div>
             <div className="img4">
                 <div className="img-set1">
-                    <img className="img1"src={img[0]} alt="Logo" width="350" height="300"/>
+                    <img className="img1"src={img[0]} alt="Logo" width="35%" height="35%"/>
                     <div className="title-background">
                         <h1 className="sub-title1">Taking Your Fuel Management Needs</h1>
                         <h1 className="sub-title1">To</h1>
@@ -28,7 +28,7 @@ function Home() {
                         </div>
                         <Link to ="/login"> <button className="btn-signup">Sign Up Here</button> </Link>
                     </div>
-                    <img className="img1"src={img[1]} alt="Logo" width="350" height="300"/>
+                    <img className="img1"src={img[1]} alt="Logo" width="30%" height="10%"/>
                 </div>
             </div>
         </div>
