908b6ee07ed0088202826c68717e362f0245802e
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-07-11 16:15:42 -0500

    create works

--- client1/src/App.js
+++ client1/src/App.js
@@ -6,7 +6,7 @@ import Login from './containers/Login';
 import Register from './containers/Register';
 import Fuel from './containers/Fuel';
 import Profile from './containers/Profile';
-import AboutUs from './containers/AboutUs';
+import AboutUs from './containers/aboutUs';
 import Dashboard from './containers/Dashboard';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
