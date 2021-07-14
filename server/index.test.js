f093628f9af7c9f66adb57b8136cc9f982dc011b
mathews1193 <mathews1193@yahoo.com>
2021-07-13 21:28:59 -0500

    added jest lib / created test script for server

--- server/index.test.js
+++ server/index.test.js
@@ -1,16 +1,3 @@
-import { render } from '@testing-library/react';
-import axios from 'axios';
-import Index from './index';
+test('Hello World', () =>{
 
-jest.mock('axios');
-
-test('should fetch orders', () => {
-  const orders = [{name: 'Bob'}];
-  const resp = {data: orders};
-  axios.get.mockResolvedValue(resp);
-
-  // or you could use the following depending on your use case:
-  // axios.get.mockImplementation(() => Promise.resolve(resp))
-
-  return Index.all().then(data => expect(data).toEqual(orders));
 });
