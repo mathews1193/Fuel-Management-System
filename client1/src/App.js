c3b59b1ca911044ec2c5944591065fa432d9f715
JordanYu21 <jyu32000@gmail.com>
2021-07-15 22:42:35 -0500

    fixed about us path

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useState } from 'react';
 import './App.css';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Register from './containers/Register';
 import Fuel from './containers/Fuel';
 import Profile from './containers/Profile';
+import AboutUs from './containers/aboutUs';
 import Dashboard from './containers/Dashboard';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const [token, setToken] = useState();
   return (
     <div className="App">
       <Router>
@@ -21,6 +24,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/fuel-quote" component={Fuel} />
           <Route path="/client-profile" component={Profile} />
+          <Route path="/about-us" component={AboutUs} />
           <Route path="/dashboard" component={Dashboard} />
         </div>
         <Footer />
