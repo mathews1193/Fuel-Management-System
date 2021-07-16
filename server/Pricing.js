605d5d96ed2943a6cd2b003b8c20ae864896f22c
mathews1193 <mathews1193@yahoo.com>
2021-07-15 21:54:38 -0500

    created pricing class for fuel total amount

---
+++ server/Pricing.js
@@ -0,0 +1,10 @@
+class pricing {
+    constructor(suggestedPrice, totalAmount, gallonsRequested, state) 
+    {
+      this.suggestedPrice = suggestedPrice;
+      this.totalAmount = totalAmount;
+      this.gallonsRequested = gallonsRequested;
+      this.state = state; 
+    }
+
+  }
--- server/index.test.js
+++ server/index.test.js
@@ -1,3 +1,33 @@
-test('Hello World', () =>{
+const Index = require("./index");
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
+
+// configuration of the mysql database // 
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password:'password',
+    database:'fuel-managment-system'
+})
+
+
+describe("Should perform CRUD on fuel quotes", () => {
+  test("create fuel quote", async() => {
+      
+    app.get("/fuelquotes", (req, res) => {
+      const result = db.query("SELECT * FROM fuelquotes")
+      console.log(result);
+    });
+    
+    expect(result).toNotBeNull();
+  });
+  
 });
+   
+
+ 
